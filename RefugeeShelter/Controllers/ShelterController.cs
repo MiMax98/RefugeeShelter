33ed25f4b7423619a97032f52e64f8fb80af6af6
Michał Witczak <michalwitczak19@gmail.com>
2022-07-25 19:51:31 +0200

    Add project files.

---
+++ RefugeeShelter/Controllers/ShelterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RefugeeShelter.Models;
+
+namespace RefugeeShelter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SheltersController : ControllerBase
+    {
+        [HttpGet(Name = "GetShelters")]
+        public IEnumerable<Shelter> Get()
+        {
+            var shelters = new List<Shelter>
+            {
+                new Shelter
+                {
+                    ShelterId = 1,
+                    ShelterName = "Pokój dwuosobowy",
+                    Address = "Asdf 13",
+                    City = "Rzeszów",
+                    Latitude = 23.456,
+                    Longitude = 14.569
+                }
+            };
+            return shelters;
+        }
+    }
+}
---
+++ RefugeeShelter/Models/Shelter.cs
@@ -0,0 +1,15 @@
+﻿namespace RefugeeShelter.Models
+{
+    public class Shelter
+    {
+        public int ShelterId { get; set; }
+        public string ShelterName { get; set; } = "";
+        public string? Description { get; set; }
+        public string City { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string ZipCode { get; set; } = "00-000";
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string? Base64Image { get; set; }
+    }
+}
